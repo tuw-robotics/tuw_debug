5e818125669ea64672d56056689649f810b15e10
Markus Bader <markus.bader@tuwien.ac.at>
2024-02-14 10:18:24 +0100

    first push

---
+++ common/include/tuw_debug/debug.hpp
@@ -0,0 +1,18 @@
+/**
+ * @author Markus Bader <markus.bader@tuwien.ac.at>
+ **/
+#ifndef TUW_DEBUG__DEBUG_HPP
+#define TUW_DEBUG__DEBUG_HPP
+
+namespace tuw
+{
+    /**
+     * Debug
+     */
+    class Debug
+    {
+    public:
+        Debug();
+    };
+};
+#endif // TUW_DEBUG__TUW_DEBUG_HPP
